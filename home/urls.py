fafbfe804cbc107d71597e144f97f77a1ea816a1
DavidGitau <86714248+DavidGitau@users.noreply.github.com>
2022-08-14 23:37:04 +0300

    Project list view complete

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-from .views import home_view
+from .views import (
+    home_view,
+    ProjectList,
+)
 
 app_name = 'home'
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('projects/', ProjectList.as_view(), name='project-list'),
 ]
