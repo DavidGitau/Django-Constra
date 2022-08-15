a5828a950aaa5bf266d64a83d3ba89ee5d02dd9f
DavidGitau <86714248+DavidGitau@users.noreply.github.com>
2022-08-15 16:20:37 +0300

    Team model complete

--- home/models.py
+++ home/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
-class Category(models.Model):
+
+class CommonInfo(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
     
     class Meta:
+        abstract = True
+    
+class Category(CommonInfo):
+    class Meta:
         verbose_name_plural = 'Categories'
 
 
-class Fact(models.Model):
-    name = models.CharField(max_length=100)
+class Fact(CommonInfo):
     number = models.IntegerField()
     image = models.ImageField(upload_to='images/icon-image/')
 
-    def __str__(self):
-        return self.name
-
-class Project(models.Model):
-    name = models.CharField(max_length=100)
+class Project(CommonInfo):
     about = models.TextField(default='Morbi turpis nisl, auctor ut nisl vel, pellentesque euismod nunc nunc accumsan imperdiet.')
     location = models.CharField(max_length=100, default='McLean, VA')
     client = models.CharField(max_length=100, default='Pransbay Powers Authority')
@@ -28,5 +28,7 @@ class Project(models.Model):
     image = models.ImageField(upload_to='images/projects/')
     categories = models.ManyToManyField(Category)
 
-    def __str__(self):
-        return self.name
+class Team(CommonInfo):
+    about = models.TextField(default='Morbi turpis nisl, auctor ut nisl vel, pellentesque euismod nunc nunc accumsan imperdiet.')
+    position = models.CharField(max_length=100, default='Dlarke Pelli Incorp')
+    image = models.ImageField(upload_to='images/team/')
