4367e805481723f81395361a1d191e4b9cec0e82
DavidGitau <86714248+DavidGitau@users.noreply.github.com>
2022-08-15 16:27:15 +0300

    Testimonial model complete

--- home/models.py
+++ home/models.py
@@ -29,6 +29,12 @@ class Project(CommonInfo):
     categories = models.ManyToManyField(Category)
 
 class Team(CommonInfo):
-    about = models.TextField(default='Morbi turpis nisl, auctor ut nisl vel, pellentesque euismod nunc nunc accumsan imperdiet.')
-    position = models.CharField(max_length=100, default='Dlarke Pelli Incorp')
+    about = models.TextField(default='Nats Stenman began his career in construction with boots on the ground')
+    position = models.CharField(max_length=100, default='Innovation Officer')
     image = models.ImageField(upload_to='images/team/')
+
+class Testimonial(CommonInfo):
+    about = models.TextField(
+    default='Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.')
+    position = models.CharField(max_length=100, default='CEO, First Choice Group')
+    image = models.ImageField(upload_to='images/clients/')
